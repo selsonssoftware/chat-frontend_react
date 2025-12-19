01e625f2efd8ae5cb33847c3cd5c6c2db3b3b5b6
Jeffrey Solomon <jeffreysolomon.official@gmail.com>
2025-12-19 17:59:55 +0530

    feat:deploy vercel

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000" : "http://localhost:3000",
+  baseURL: import.meta.env.MODE === "development" ? "https://festive-northcutt.147-93-19-140.plesk.page" : "https://festive-northcutt.147-93-19-140.plesk.page",
   withCredentials: true,
 });
